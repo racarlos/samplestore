d28d5d944d429eb299ebb63dbda060cf8ddadc3d
racarlos <rarcarlos09@gmail.com>
2025-06-20 21:25:33 +0800

    initial commitL: scaffold and setup nativewind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+	return (
+		<View className="flex-1 items-center justify-center bg-red-300">
+			<Text className="text-white">Home screen</Text>
+		</View>
+	);
+}
